615b2184afe6196585e87e9a2b8a3ae4d5bd906d
Evangelos Aktoudianakis <evangelos.aktoudianakis@amido.com>
2021-03-12 14:14:06 +0000

    Added basecontroller action to retrieve correlationId + unit tests

--- BaseApi/V1/Controllers/BaseController.cs
+++ BaseApi/V1/Controllers/BaseController.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
@@ -6,9 +7,19 @@ namespace BaseApi.V1.Controllers
 {
     public class BaseController : Controller
     {
+        private string _correlationid = "x-correlationId";
+
         public BaseController()
         {
             ConfigureJsonSerializer();
+        }
+
+        public string GetCorrelationId()
+        {
+            if (HttpContext.Request.Headers[_correlationid].Count == 0)
+                throw new KeyNotFoundException("Request is missing a correlationId");
+
+            return HttpContext.Request.Headers[_correlationid];
         }
 
         public static void ConfigureJsonSerializer()
--- BaseApi/V1/Infrastructure/CorrelationMiddleware.cs
+++ BaseApi/V1/Infrastructure/CorrelationMiddleware.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Globalization;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
@@ -23,7 +22,7 @@ namespace BaseApi.V1.Infrastructure
                 context.Request.Headers[_correlationid] = Guid.NewGuid().ToString();
             }
 
-            if(_next != null)
+            if (_next != null)
                 await _next(context).ConfigureAwait(false);
         }
     }
