36ef823b61b5724226e7efac6422fa67cd0624c8
Evangelos Aktoudianakis <evangelos.aktoudianakis@amido.com>
2021-03-19 16:26:00 +0000

    added newline after argument

--- SearchApi/terraform/development/main.tf
+++ SearchApi/terraform/development/main.tf
@@ -16,7 +16,7 @@ data "aws_caller_identity" "current" {}
 
 data "aws_region" "current" {}
 
-locals {   
+locals {
    parameter_store = "arn:aws:ssm:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:parameter"
 }
 
