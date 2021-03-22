bf63990ef9ccaa90e1efaa74e3f5771afdef33be
Evangelos Aktoudianakis <evangelos.aktoudianakis@amido.com>
2021-03-22 15:19:22 +0000

    Added get request

---
+++ SearchApi/V1/Domain/GetPersonRequest.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SearchApi.V1.Domain
+{
+    public class GetPersonRequest
+    {
+        [FromQuery(Name = "postcode")]
+        public string PostCode { get; set; }
+
+        [FromQuery(Name = "llpg-ref")]
+        public string LLPGReference { get; set; }
+
+        [FromQuery(Name = "prop-ref")]
+        public string PropertyReference { get; set; }
+    }
+}
