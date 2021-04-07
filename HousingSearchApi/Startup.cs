43273c3372a0be8ecf749959f3b7c8082748bec7
Evangelos Aktoudianakis <evangelos.aktoudianakis@amido.com>
2021-04-07 16:59:18 +0100

    Used .NET Cors instead of my own

--- HousingSearchApi/Startup.cs
+++ HousingSearchApi/Startup.cs
@@ -43,6 +43,8 @@ namespace HousingSearchApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public static void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors();
+
             services
                 .AddMvc()
                 .AddNewtonsoftJson(x =>
@@ -163,10 +165,14 @@ namespace HousingSearchApi
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public static void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseCors(builder => builder
+                .AllowAnyOrigin()
+                .AllowAnyHeader()
+                .AllowAnyMethod());
+
             app.UseXRay("housing-search-api");
 
             app.UseCorrelation();
-            app.UseCORS();
 
             if (env.IsDevelopment())
             {
