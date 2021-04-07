3128d450153a167e3240093c2d99bc45d04c6e54
Evangelos Aktoudianakis <80248889+evak6979@users.noreply.github.com>
2021-04-07 15:52:31 +0100

    Update Constants.cs

--- HousingSearchApi/V1/Infrastructure/Constants.cs
+++ HousingSearchApi/V1/Infrastructure/Constants.cs
@@ -3,6 +3,9 @@ namespace HousingSearchApi.V1.Infrastructure
     public static class Constants
     {
         public const string CorrelationId = "x-correlation-id";
+        public const string CorsHeader = "Access-Control-Allow-Origin";
+        public const string CorsHeaderMethods = "Access-Control-Allow-Methods";
+        public const string CorsHeaderHeaders = "Access-Control-Allow-Headers";
 
         // Elastic Search
         public const string EsIndex = "hackney_persons";
