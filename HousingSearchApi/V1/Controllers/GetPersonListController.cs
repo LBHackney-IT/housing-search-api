8d99fb8b6839d20f131fffe37f1c3bf9650e157b
Evangelos Aktoudianakis <evangelos.aktoudianakis@amido.com>
2021-04-15 12:09:25 +0100

    Reverted size change

--- HousingSearchApi/V1/Controllers/GetPersonListController.cs
+++ HousingSearchApi/V1/Controllers/GetPersonListController.cs
@@ -48,7 +48,7 @@ namespace HousingSearchApi.V1.Controllers
             {
                 var personsSearchResult = await _getPersonListUseCase.ExecuteAsync(request);
                 var apiResponse = new APIResponse<GetPersonListResponse>(personsSearchResult);
-                apiResponse.Total = personsSearchResult.Total();
+                apiResponse.Total = personsSearchResult.Persons?.Count ?? 0;
 
                 return new OkObjectResult(apiResponse);
             }
