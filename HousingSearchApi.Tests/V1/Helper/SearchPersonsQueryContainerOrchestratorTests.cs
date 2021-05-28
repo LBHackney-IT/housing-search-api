643a7ff004769c3263dc55421e24c3fb1320a810
Evangelos Aktoudianakis <evangelos.aktoudianakis@amido.com>
2021-05-28 14:44:23 +0100

    Fixed broken test

--- HousingSearchApi.Tests/V1/Helper/SearchPersonsQueryContainerOrchestratorTests.cs
+++ HousingSearchApi.Tests/V1/Helper/SearchPersonsQueryContainerOrchestratorTests.cs
@@ -1,11 +1,9 @@
 using System.Linq;
 using FluentAssertions;
 using HousingSearchApi.V1.Boundary.Requests;
-using HousingSearchApi.V1.Gateways;
 using HousingSearchApi.V1.Gateways.Models;
 using HousingSearchApi.V1.Infrastructure;
 using HousingSearchApi.V1.Interfaces;
-using Moq;
 using Nest;
 using Xunit;
 
@@ -14,24 +12,21 @@ namespace HousingSearchApi.Tests.V1.Helper
     public class SearchPersonsQueryContainerOrchestratorTests
     {
         private SearchPersonsQueryContainerOrchestrator _sut;
-        private Mock<IWildCardAppenderAndPrepender> _mockAppender;
 
         public SearchPersonsQueryContainerOrchestratorTests()
         {
-            _mockAppender = new Mock<IWildCardAppenderAndPrepender>();
-
-            _sut = new SearchPersonsQueryContainerOrchestrator(_mockAppender.Object);
+            _sut = new SearchPersonsQueryContainerOrchestrator(new WildCardAppenderAndPrepender());
         }
 
         [Fact]
-        public void ShouldReturnOrSearchNamesSurnames()
+        public void ShouldReturnSearchPhraseQueryStringContainer()
         {
             // Arrange + Act
             var result = _sut.Create(new GetPersonListRequest { SearchText = "abc" },
                 new QueryContainerDescriptor<QueryablePerson>());
 
             // Assert
-            (result as IQueryContainer).Bool.Should.Count().Should().Be(2);
+            (result as IQueryContainer).QueryString.Should().NotBeNull();
         }
     }
 }
