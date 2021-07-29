f1fb34d73ace0405d173bd3392932a5c46a9522b
LBHMGeorgieva <mirela.georgieva@hackney.gov.uk>
2021-07-29 09:21:33 +0100

    Increase ES nodes and instance size

--- terraform/staging/main.tf
+++ terraform/staging/main.tf
@@ -43,7 +43,7 @@ module "elasticsearch_db_staging" {
   subnet_ids       = [tolist(data.aws_subnet_ids.staging.ids)[0]]
   project_name     = "housing-search-api"
   es_version       = "7.8"
-  encrypt_at_rest  = "false"
+  encrypt_at_rest  = "true"
   instance_type    = "t3.small.elasticsearch"
   instance_count   = "1"
   ebs_enabled      = "true"
