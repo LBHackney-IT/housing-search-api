bef223d33e65ffd0b38cd2bbe9611cd556c5ab88
Evangelos Aktoudianakis <evangelos.aktoudianakis@amido.com>
2021-08-16 21:22:17 +0100

    Added tests for AssetController

--- HousingSearchApi.Tests/V1/Controllers/GetAssetListControllerTests.cs
+++ HousingSearchApi.Tests/V1/Controllers/GetAssetListControllerTests.cs
@@ -14,16 +14,16 @@ namespace HousingSearchApi.Tests.V1.Controllers
     [Collection("LogCall collection")]
     public class GetTenureListControllerTests
     {
-        private readonly Mock<IGetTenureListUseCase> _mockGetTenureListUseCase;
-        private readonly GetTenureListController _classUnderTest;
+        private readonly Mock<IGetAssetListUseCase> _mockGetAssetListUseCase;
+        private readonly GetAssetListController _classUnderTest;
 
 
         public GetTenureListControllerTests()
         {
             new LogCallAspectFixture().RunBeforeTests();
 
-            _mockGetTenureListUseCase = new Mock<IGetTenureListUseCase>();
-            _classUnderTest = new GetTenureListController(_mockGetTenureListUseCase.Object);
+            _mockGetAssetListUseCase = new Mock<IGetAssetListUseCase>();
+            _classUnderTest = new GetAssetListController(_mockGetAssetListUseCase.Object);
         }
 
         [Fact]
@@ -31,14 +31,14 @@ namespace HousingSearchApi.Tests.V1.Controllers
         {
             // given
             var request = new HousingSearchRequest();
-            var response = new GetTenureListResponse();
-            _mockGetTenureListUseCase.Setup(x => x.ExecuteAsync(request)).ReturnsAsync(response);
+            var response = new GetAssetListResponse();
+            _mockGetAssetListUseCase.Setup(x => x.ExecuteAsync(request)).ReturnsAsync(response);
 
             // when
-            await _classUnderTest.GetTenureList(request).ConfigureAwait(false);
+            await _classUnderTest.GetAssetList(request).ConfigureAwait(false);
 
             // then
-            _mockGetTenureListUseCase.Verify(x => x.ExecuteAsync(request), Times.Once);
+            _mockGetAssetListUseCase.Verify(x => x.ExecuteAsync(request), Times.Once);
         }
 
         [Fact(Skip = "This test fails because the call to the use case returns a NotFound exception, which is caught and converted into a BadRequest response. Need to validate this is the required functionality.")]
@@ -48,11 +48,11 @@ namespace HousingSearchApi.Tests.V1.Controllers
             var request = new HousingSearchRequest();
             var notFoundException = new NotFoundException();
 
-            _mockGetTenureListUseCase.Setup(x => x.ExecuteAsync(request))
+            _mockGetAssetListUseCase.Setup(x => x.ExecuteAsync(request))
                 .Throws(notFoundException);
 
             // when
-            var result = await _classUnderTest.GetTenureList(request).ConfigureAwait(false);
+            var result = await _classUnderTest.GetAssetList(request).ConfigureAwait(false);
 
             // then
             result.Should().BeOfType(typeof(NotFoundObjectResult));
