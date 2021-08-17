e1767a7c6b69a4cb466239093092ab5f7cd7beaa
Evangelos Aktoudianakis <evangelos.aktoudianakis@amido.com>
2021-08-17 13:56:55 +0100

    Refactoring

--- HousingSearchApi/V1/Interfaces/PersonQueryGenerator.cs
+++ HousingSearchApi/V1/Interfaces/PersonQueryGenerator.cs
@@ -5,42 +5,6 @@ using Nest;
 
 namespace HousingSearchApi.V1.Interfaces
 {
-    public interface IQueryFactory
-    {
-        IQueryGenerator<T> CreateQuery<T>(HousingSearchRequest request) where T : class;
-    }
-
-    public class QueryFactory : IQueryFactory
-    {
-        private readonly IWildCardAppenderAndPrepender _wildCardAppenderAndPrepender;
-
-        public QueryFactory(IWildCardAppenderAndPrepender wildCardAppenderAndPrepender)
-        {
-            _wildCardAppenderAndPrepender = wildCardAppenderAndPrepender;
-        }
-
-        public IQueryGenerator<T> CreateQuery<T>(HousingSearchRequest request) where T : class
-        {
-            if (typeof(T) == typeof(QueryablePerson))
-            {
-                return (IQueryGenerator<T>)new PersonQueryGenerator(_wildCardAppenderAndPrepender);
-            }
-
-            if (typeof(T) == typeof(QueryableTenure))
-            {
-                return (IQueryGenerator<T>) new TenureQueryGenerator(_wildCardAppenderAndPrepender);
-            }
-
-            if (typeof(T) == typeof(QueryableAsset))
-            {
-                return (IQueryGenerator<T>) new AssetQueryGenerator(_wildCardAppenderAndPrepender);
-            }
-
-
-            throw new System.NotImplementedException($"Query type {typeof(T)} is not implemented");
-        }
-    }
-
     public class PersonQueryGenerator : IQueryGenerator<QueryablePerson>
     {
         private readonly IWildCardAppenderAndPrepender _wildCardAppenderAndPrepender;
---
+++ HousingSearchApi/V1/Interfaces/QueryFactory.cs
@@ -0,0 +1,37 @@
+using HousingSearchApi.V1.Boundary.Requests;
+using HousingSearchApi.V1.Gateways.Models;
+using HousingSearchApi.V1.Infrastructure;
+
+namespace HousingSearchApi.V1.Interfaces
+{
+    public class QueryFactory : IQueryFactory
+    {
+        private readonly IWildCardAppenderAndPrepender _wildCardAppenderAndPrepender;
+
+        public QueryFactory(IWildCardAppenderAndPrepender wildCardAppenderAndPrepender)
+        {
+            _wildCardAppenderAndPrepender = wildCardAppenderAndPrepender;
+        }
+
+        public IQueryGenerator<T> CreateQuery<T>(HousingSearchRequest request) where T : class
+        {
+            if (typeof(T) == typeof(QueryablePerson))
+            {
+                return (IQueryGenerator<T>)new PersonQueryGenerator(_wildCardAppenderAndPrepender);
+            }
+
+            if (typeof(T) == typeof(QueryableTenure))
+            {
+                return (IQueryGenerator<T>) new TenureQueryGenerator(_wildCardAppenderAndPrepender);
+            }
+
+            if (typeof(T) == typeof(QueryableAsset))
+            {
+                return (IQueryGenerator<T>) new AssetQueryGenerator(_wildCardAppenderAndPrepender);
+            }
+
+
+            throw new System.NotImplementedException($"Query type {typeof(T)} is not implemented");
+        }
+    }
+}
