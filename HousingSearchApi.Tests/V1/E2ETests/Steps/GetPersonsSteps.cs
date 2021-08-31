831ed816114f39ba6888549a833ccbfa4b04ec41
golosowa.anna <golosowa.anna@gmail.com>
2021-08-31 16:44:00 +0300

    Code clean up

--- HousingSearchApi.Tests/V1/E2ETests/Steps/GetPersonsSteps.cs
+++ HousingSearchApi.Tests/V1/E2ETests/Steps/GetPersonsSteps.cs
@@ -32,7 +32,7 @@ namespace HousingSearchApi.Tests.V1.E2ETests.Steps
 
         public async Task WhenSearchingByFirstAndLastName(string firstName, string lastName)
         {
-            _lastResponse = await _httpClient.GetAsync(new Uri($"api/v1/search/persons?searchText={firstName}%20{lastName}&pageSize=1000", UriKind.Relative)).ConfigureAwait(false);
+            _lastResponse = await _httpClient.GetAsync(new Uri($"api/v1/search/persons?searchText={firstName}%20{lastName}", UriKind.Relative)).ConfigureAwait(false);
         }
 
         public async Task WhenAPageSizeIsProvided(int pageSize)
