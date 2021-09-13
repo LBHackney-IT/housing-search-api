c3713a468ae823a93fc679df681621b054e3acbf
Alex Demetriou <alex.demetriou@hackney.gov.uk>
2021-09-13 17:11:04 +0100

    Tidy up + remove redundant type check

--- HousingSearchApi/V1/Interfaces/AssetQueryGenerator.cs
+++ HousingSearchApi/V1/Interfaces/AssetQueryGenerator.cs
@@ -19,35 +19,28 @@ namespace HousingSearchApi.V1.Interfaces
 
         public QueryContainer Create(HousingSearchRequest request, QueryContainerDescriptor<QueryableAsset> q)
         {
-            if (!(request is HousingSearchRequest housingSearchRequest)) return null;
-
-            var queryContainer = new QueryContainer();
             var filters = new List<Func<QueryContainerDescriptor<QueryableAsset>, QueryContainer>>();
+            var listOfWildCardedWords = _wildCardAppenderAndPrepender.Process(request.SearchText);
 
-            var listOfWildCardedWords = _wildCardAppenderAndPrepender.Process(housingSearchRequest.SearchText);
-
-            Func<QueryContainerDescriptor<QueryableAsset>, QueryContainer> filterBySearchTextContainer =
-                (containerDescriptor) => containerDescriptor.QueryString(q => q.Query($"({string.Join(" AND ", listOfWildCardedWords)}) " + string.Join(' ', listOfWildCardedWords))
+            #region Filter definitions
+            QueryContainer FilterBySearchTextContainer(QueryContainerDescriptor<QueryableAsset> containerDescriptor) =>
+                containerDescriptor
+                    .QueryString(qs => qs.Query($"({string.Join(" AND ", listOfWildCardedWords)}) " + string.Join(' ', listOfWildCardedWords))
                     .Fields(f => f.Field("*"))
                     .Type(TextQueryType.MostFields));
 
-            filters.Add(filterBySearchTextContainer);
+            QueryContainer FilterByTypeContainer(QueryContainerDescriptor<QueryableAsset> containerDescriptor) =>
+                containerDescriptor
+                    .QueryString(qs => qs.Query(string.Join(' ', request.AssetTypes.Split(",")))
+                    .Fields(f => f.Field(asset => asset.AssetType))
+                    .Type(TextQueryType.MostFields));
+            #endregion
 
-            if (!string.IsNullOrWhiteSpace(housingSearchRequest.AssetTypes))
-            {
-                var types = housingSearchRequest.AssetTypes.Split(",");
+            filters.Add(FilterBySearchTextContainer);
+            if (!string.IsNullOrWhiteSpace(request.AssetTypes))
+                filters.Add(FilterByTypeContainer);
 
-                Func<QueryContainerDescriptor<QueryableAsset>, QueryContainer> filterByTypeContainer =
-                    (containerDescriptor) => containerDescriptor.QueryString(q => q.Query(string.Join(' ', types))
-                        .Fields(f => f.Field(asset => asset.AssetType))
-                        .Type(TextQueryType.MostFields));
-
-                filters.Add(filterByTypeContainer);
-            }
-
-            queryContainer = q.Bool(bq => bq.Filter(filters.ToArray()));
-
-            return queryContainer;
+            return q.Bool(bq => bq.Filter(filters.ToArray()));
         }
     }
 }
