e8eed3f3f90f4b33ce85583a83a7bca12b7fbb1a
Alex Demetriou <alex.demetriou@hackney.gov.uk>
2021-09-22 12:07:53 +0100

    Remove unused QueryableTenuredAsset
    The tenuredAsset is not present in real data

--- HousingSearchApi/V1/Gateways/Models/Assets/QueryableTenure.cs
+++ HousingSearchApi/V1/Gateways/Models/Assets/QueryableTenure.cs
@@ -6,14 +6,18 @@ namespace HousingSearchApi.V1.Gateways.Models.Assets
     {
         public Domain.Asset.Tenure Create()
         {
-            return Domain.Asset.Tenure.Create(Id, PaymentReference, StartOfTenureDate, EndOfTenureDate,
-                TenuredAsset, Type);
+            return Domain.Asset.Tenure.Create(
+                Id,
+                PaymentReference,
+                StartOfTenureDate,
+                EndOfTenureDate,
+                Type
+            );
         }
 
         [Text(Name = "id")]
         public string Id { get; set; }
         public string PaymentReference { get; set; }
-        public QueryableTenuredAsset TenuredAsset { get; set; }
         public string StartOfTenureDate { get; set; }
         public string EndOfTenureDate { get; set; }
         public string Type { get; set; }
