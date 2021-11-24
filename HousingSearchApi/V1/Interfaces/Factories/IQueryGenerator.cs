dbd4f62ab90efded3c42c9d2775851e6f0d4326b
Hamid Jolany <hamid.jolany@hackney.gov.uk>
2021-11-24 13:33:48 +0330

    Format fixing

--- HousingSearchApi/V1/Interfaces/Factories/IQueryGenerator.cs
+++ HousingSearchApi/V1/Interfaces/Factories/IQueryGenerator.cs
@@ -5,6 +5,6 @@ namespace HousingSearchApi.V1.Interfaces.Factories
 {
     public interface IQueryGenerator<T> where T : class
     {
-        QueryContainer Create<TRequest>(TRequest request,QueryContainerDescriptor<T> q);
+        QueryContainer Create<TRequest>(TRequest request, QueryContainerDescriptor<T> q);
     }
 }
