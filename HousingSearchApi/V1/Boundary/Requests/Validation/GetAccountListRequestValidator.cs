f2b8549238bf1d3b58b3c2b480e9019106c208a7
Hamid Jolany <hamid.jolany@hackney.gov.uk>
2021-11-26 16:49:48 +0330

    comments removed

--- HousingSearchApi/V1/Boundary/Requests/Validation/GetAccountListRequestValidator.cs
+++ HousingSearchApi/V1/Boundary/Requests/Validation/GetAccountListRequestValidator.cs
@@ -7,10 +7,6 @@ namespace HousingSearchApi.V1.Boundary.Requests.Validation
     {
         public GetAccountListRequestValidator()
         {
-            /*RuleFor(x => x.SearchText).NotNull()
-                                      .NotEmpty()
-                                      .MinimumLength(2)
-                                      .NotXssString();*/
             RuleFor(x => x.PageSize).GreaterThan(0);
             RuleFor(x => x.SortBy).NotXssString();
         }
