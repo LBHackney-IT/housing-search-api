37916543d44576abc2d4e6aacf85c46065008872
Hamid Jolany <hamid.jolany@hackney.gov.uk>
2021-11-29 20:09:23 +0330

    Some validation rules changed Aws_vpc changed

--- HousingSearchApi/V1/Boundary/Requests/Validation/GetAccountListRequestValidator.cs
+++ HousingSearchApi/V1/Boundary/Requests/Validation/GetAccountListRequestValidator.cs
@@ -7,6 +7,35 @@ namespace HousingSearchApi.V1.Boundary.Requests.Validation
     {
         public GetAccountListRequestValidator()
         {
+            RuleFor(x => x.SearchText).NotNull()
+                .DependentRules(() =>
+                {
+                    RuleFor(x => x.TargetId).Null();
+                    RuleFor(x => x.TargetId).Empty();
+                })
+                .NotEmpty()
+                .DependentRules(() =>
+                {
+                    RuleFor(x => x.TargetId).Null();
+                    RuleFor(x => x.TargetId).Empty();
+                })
+                .MaximumLength(2)
+                .NotXssString();
+
+            RuleFor(x => x.TargetId).NotNull()
+                .DependentRules(() =>
+                {
+                    RuleFor(x => x.SearchText).Null();
+                    RuleFor(x => x.SearchText).Empty();
+                })
+                .NotEmpty()
+                .DependentRules(() =>
+                {
+                    RuleFor(x => x.SearchText).Null();
+                    RuleFor(x => x.SearchText).Empty();
+                })
+                .NotXssString();
+
             RuleFor(x => x.PageSize).GreaterThan(0);
             RuleFor(x => x.SortBy).NotXssString();
         }
