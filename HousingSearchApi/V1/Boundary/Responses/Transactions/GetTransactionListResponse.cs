afce6a017e1f185a5042e348fda3fed93894cb57
Hamid Jolany <hamid.jolany@hackney.gov.uk>
2021-12-03 02:42:24 +0330

    Transaction Querygenerator amendment due to search by targetId

--- HousingSearchApi/V1/Boundary/Responses/Transactions/GetTransactionListResponse.cs
+++ HousingSearchApi/V1/Boundary/Responses/Transactions/GetTransactionListResponse.cs
@@ -10,12 +10,7 @@ namespace HousingSearchApi.V1.Boundary.Responses.Transactions
 
         private GetTransactionListResponse(long total, IEnumerable<TransactionResponse> transactions)
         {
-            if (total < 0)
-            {
-                throw new ArgumentException("Transactions count should be greater than or equals to 0.", nameof(total));
-            }
-
-            Total = total;
+            Total = total < 0 ? throw new ArgumentException("Transactions count should be greater than or equals to 0.", nameof(total)) : total;
             Transactions = new List<TransactionResponse>(transactions ??
                                                          throw new ArgumentNullException(nameof(transactions), "Transactions cannot be null. Provide empty list if no transactions was found."));
         }
