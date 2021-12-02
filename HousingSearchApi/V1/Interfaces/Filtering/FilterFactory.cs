8dff5c8ed972d6a00ca9c0773d3dc9b3fd6dc250
AnnaGolosowa <golosowa.anna@gmail.com>
2021-12-02 16:35:52 +0300

    Fix e2e tests.

--- HousingSearchApi/V1/Interfaces/Filtering/FilterFactory.cs
+++ HousingSearchApi/V1/Interfaces/Filtering/FilterFactory.cs
@@ -12,26 +12,26 @@ namespace HousingSearchApi.V1.Interfaces.Filtering
         /// </summary>
         public QueryContainer Filter<T, TRequest>(TRequest request, QueryContainerDescriptor<T> q) where T : class where TRequest : class
         {
-            if (!(request is GetTransactionListRequest transactionSearchRequest))
-                throw new ArgumentNullException(nameof(request));
+            // ToDo: implement factory for choosing type of filter
+            if (request is GetTransactionListRequest transactionSearchRequest &&
+                q is QueryContainerDescriptor<QueryableTransaction> transactionDescriptor)
+            {
+                if (transactionSearchRequest.StartDate == DateTime.MinValue &&
+                                                transactionSearchRequest.EndDate == DateTime.MinValue)
+                    return null;
 
-            if (!(q is QueryContainerDescriptor<QueryableTransaction> transactionDescriptor))
-                throw new ArgumentNullException(nameof(q));
+                if (transactionSearchRequest.StartDate == DateTime.MinValue)
+                    transactionSearchRequest.StartDate = DateTime.UnixEpoch;
 
-            if (transactionSearchRequest.StartDate == DateTime.MinValue &&
-                                          transactionSearchRequest.EndDate == DateTime.MinValue)
-                return null;
+                if (transactionSearchRequest.EndDate == DateTime.MinValue)
+                    transactionSearchRequest.EndDate = DateTime.UtcNow;
 
-            if (transactionSearchRequest.StartDate == DateTime.MinValue)
-                transactionSearchRequest.StartDate = DateTime.UnixEpoch;
-
-            if (transactionSearchRequest.EndDate == DateTime.MinValue)
-                transactionSearchRequest.EndDate = DateTime.UtcNow;
-
-            return transactionDescriptor.DateRange(c => c
-                .Field(p => p.TransactionDate)
-                .GreaterThanOrEquals(transactionSearchRequest.StartDate)
-                .LessThanOrEquals(transactionSearchRequest.EndDate));
+                return transactionDescriptor.DateRange(c => c
+                    .Field(p => p.TransactionDate)
+                    .GreaterThanOrEquals(transactionSearchRequest.StartDate)
+                    .LessThanOrEquals(transactionSearchRequest.EndDate));
+            }
+            return null;
         }
     }
 }
