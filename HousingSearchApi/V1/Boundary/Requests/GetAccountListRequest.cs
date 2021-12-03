5251c4109c7ac1fe2ed34d1b5e48aedea1b2ec8c
Hamid Jolany <hamid.jolany@hackney.gov.uk>
2021-12-03 13:16:48 +0330

    Format Fixing

--- HousingSearchApi/V1/Boundary/Requests/GetAccountListRequest.cs
+++ HousingSearchApi/V1/Boundary/Requests/GetAccountListRequest.cs
@@ -3,7 +3,7 @@ using HousingSearchApi.V1.Boundary.Requests.Interfaces;
 
 namespace HousingSearchApi.V1.Boundary.Requests
 {
-    public class GetAccountListRequest : HousingSearchRequest,ISearchByTargetId
+    public class GetAccountListRequest : HousingSearchRequest, ISearchByTargetId
     {
         public Guid TargetId { get; set; }
     }
