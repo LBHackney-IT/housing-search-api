b65fb6e2a214dc55487dac8b8fc508b1c8ad1479
humulla <43747248+humulla@users.noreply.github.com>
2022-10-21 15:07:38 +0100

    using core package

--- HousingSearchApi/V1/Interfaces/IFilterQueryBuilder.cs
+++ HousingSearchApi/V1/Interfaces/IFilterQueryBuilder.cs
@@ -9,10 +9,6 @@ namespace HousingSearchApi.V1.Interfaces
         public IFilterQueryBuilder<T> WithMultipleFilterQuery(string commaSeparatedFilters, List<string> fields);
         public new QueryContainer Build(QueryContainerDescriptor<T> containerDescriptor);
 
-        public new IQueryBuilder<T> WithWildstarQuery(string searchText, List<string> fields, TextQueryType textQueryType = TextQueryType.MostFields);
 
-        public new IQueryBuilder<T> WithFilterQuery(string commaSeparatedFilters, List<string> fields, TextQueryType textQueryType = TextQueryType.MostFields);
-
-        public new IQueryBuilder<T> WithExactQuery(string searchText, List<string> fields, IExactSearchQuerystringProcessor processor = null, TextQueryType textQueryType = TextQueryType.MostFields);
     }
 }
