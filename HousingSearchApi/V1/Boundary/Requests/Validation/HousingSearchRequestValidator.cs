fa4e4e9089412e0beca02e1edb6eab08ca59100a
Huss <57395585+anefiox@users.noreply.github.com>
2022-12-01 09:15:20 +0000

    Remove validation from main validator for testing> This validation exists in the other validators.

--- HousingSearchApi/V1/Boundary/Requests/Validation/HousingSearchRequestValidator.cs
+++ HousingSearchApi/V1/Boundary/Requests/Validation/HousingSearchRequestValidator.cs
@@ -7,12 +7,12 @@ namespace HousingSearchApi.V1.Boundary.Requests.Validation
     {
         public HousingSearchRequestValidator()
         {
-            RuleFor(x => x.SearchText).NotNull()
-                                      .NotEmpty()
-                                      .MinimumLength(2)
-                                      .NotXssString();
-            RuleFor(x => x.PageSize).GreaterThan(0);
-            RuleFor(x => x.SortBy).NotXssString();
+            //RuleFor(x => x.SearchText).NotNull()
+            //                          .NotEmpty()
+            //                          .MinimumLength(2)
+            //                          .NotXssString();
+            //RuleFor(x => x.PageSize).GreaterThan(0);
+            //RuleFor(x => x.SortBy).NotXssString();
         }
     }
 }
