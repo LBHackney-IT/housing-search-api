b4bc8869a50e271611c537ee12ef04a8e385861a
Huss <57395585+anefiox@users.noreply.github.com>
2022-12-01 15:27:30 +0000

    Wrong file modified

--- HousingSearchApi/V1/Boundary/Requests/Validation/GetAssetListRequestValidator.cs
+++ HousingSearchApi/V1/Boundary/Requests/Validation/GetAssetListRequestValidator.cs
@@ -7,7 +7,8 @@ namespace HousingSearchApi.V1.Boundary.Requests.Validation
     {
         public GetAssetListRequestValidator()
         {
-            RuleFor(x => x.SearchText)
+            RuleFor(x => x.SearchText).NotNull()
+                                      .NotEmpty()
                                       .MinimumLength(2)
                                       .NotXssString();
             RuleFor(x => x.PageSize).GreaterThan(0);
