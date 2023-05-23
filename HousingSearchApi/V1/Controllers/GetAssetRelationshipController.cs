de17489c327c8be8e2f07a1e6645bd869d22c988
Dave Dean <david.dean@hackney.gov.uk>
2023-05-23 15:42:22 +0100

    Add ctr

---
+++ HousingSearchApi/V1/Controllers/GetAssetRelationshipController.cs
@@ -0,0 +1,47 @@
+using Amazon.Lambda.Core;
+using Hackney.Core.Logging;
+using HousingSearchApi.V1.Boundary.Requests;
+using HousingSearchApi.V1.Boundary.Responses;
+using HousingSearchApi.V1.Boundary.Responses.Metadata;
+using HousingSearchApi.V1.UseCase.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading.Tasks;
+
+namespace HousingSearchApi.V1.Controllers
+{
+    [ApiVersion("1")]
+    [Produces("application/json")]
+    [Route("api/v1/search/assetrelationships")]
+    [ApiController]
+    public class GetAssetRelationshipController : BaseController
+    {
+        private readonly IGetAssetRelationshipsUseCase _getAssetRelationshipsUseCase;
+
+        public GetAssetRelationshipController(IGetAssetRelationshipsUseCase getAssetRelationshipsUseCase)
+        {
+            _getAssetRelationshipsUseCase = getAssetRelationshipsUseCase;
+        }
+
+        [ProducesResponseType(typeof(APIResponse<GetAssetRelationshipsResponse>), 200)]
+        [ProducesResponseType(typeof(APIResponse<NotFoundException>), 404)]
+        [ProducesResponseType(typeof(APIResponse<BadRequestException>), 400)]
+        [HttpGet, MapToApiVersion("1")]
+        [LogCall(Microsoft.Extensions.Logging.LogLevel.Information)]
+        public async Task<IActionResult> GetAssetRelationships([FromQuery] GetAssetRelationshipsRequest request)
+        {
+            try
+            {
+                var assetsSearchResult = await _getAssetRelationshipsUseCase.ExecuteAsync(request).ConfigureAwait(false);
+
+                return new OkObjectResult(assetsSearchResult);
+            }
+            catch (Exception e)
+            {
+                LambdaLogger.Log(e.Message + e.StackTrace);
+                return new BadRequestObjectResult(e.Message);
+            }
+        }
+    }
+}
