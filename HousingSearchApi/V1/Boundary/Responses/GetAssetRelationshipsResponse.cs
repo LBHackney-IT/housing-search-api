408394620abef3be84850a9032fbf1c072616084
Dave Dean <david.dean@hackney.gov.uk>
2023-05-24 18:04:08 +0100

    Remove unused stuff

--- HousingSearchApi/V1/Boundary/Responses/GetAssetRelationshipsResponse.cs
+++ HousingSearchApi/V1/Boundary/Responses/GetAssetRelationshipsResponse.cs
@@ -5,27 +5,11 @@ namespace HousingSearchApi.V1.Boundary.Responses
 {
     public class GetAssetRelationshipsResponse
     {
-        private long _total;
-
-        public List<Asset> ParentAssets { get; set; }
         public List<Asset> ChildAssets { get; set; }
-        public List<Asset> RootAsset { get; set; }
 
         public GetAssetRelationshipsResponse()
         {
-            ParentAssets = new List<Asset>();
             ChildAssets = new List<Asset>();
-            RootAsset = new List<Asset>();
-        }
-
-        public void SetTotal(long total)
-        {
-            _total = total < 0 ? 0 : total;
-        }
-
-        public long Total()
-        {
-            return _total;
         }
     }
 }
