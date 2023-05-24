51eeb0c8740fb4ab848051e63b2b5cbbdc089ca1
Dave Dean <david.dean@hackney.gov.uk>
2023-05-24 18:51:36 +0100

    UC tests

--- HousingSearchApi.Tests/V1/UseCases/GetAssetRelationshipsUseCaseTests.cs
+++ HousingSearchApi.Tests/V1/UseCases/GetAssetRelationshipsUseCaseTests.cs
@@ -1,16 +1,12 @@
-using AutoFixture;
 using FluentAssertions;
-using Hackney.Shared.HousingSearch.Gateways.Models.Assets;
+using Hackney.Shared.HousingSearch.Domain.Asset;
 using HousingSearchApi.V1.Boundary.Requests;
 using HousingSearchApi.V1.Boundary.Responses;
-using HousingSearchApi.V1.Factories;
-using HousingSearchApi.V1.Gateways;
 using HousingSearchApi.V1.Gateways.Interfaces;
-using HousingSearchApi.V1.Helper.Interfaces;
-using HousingSearchApi.V1.Interfaces;
 using HousingSearchApi.V1.UseCase;
 using Moq;
-using Nest;
+using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -25,6 +21,46 @@ namespace HousingSearchApi.Tests.V1.UseCases
             _searchGatewayMock = new Mock<ISearchGateway>();
 
             _sut = new GetAssetRelationshipsUseCase(_searchGatewayMock.Object);
+        }
+
+        [Fact]
+        public async Task ShouldReturnValidResponseObject()
+        {
+            // Arrange
+            string parentId = Guid.NewGuid().ToString();
+
+            var assetList = new List<Asset>
+            {
+                new Asset
+                {
+                    AssetId = "TEST0001",
+                    ParentAssetIds = parentId
+                },
+                new Asset
+                {
+                    AssetId = "TEST0002",
+                    ParentAssetIds = parentId
+                }
+            };
+
+            _searchGatewayMock.Setup(x => x.GetChildAssets(Moq.It.IsAny<GetAssetRelationshipsRequest>()))
+                .ReturnsAsync(assetList);
+
+            var request = new GetAssetRelationshipsRequest
+            {
+                SearchText = parentId,
+            };
+
+            // Act
+            var result = await _sut.ExecuteAsync(request);
+
+            // Assert
+            result.Should().BeOfType<GetAssetRelationshipsResponse>();
+
+            foreach (var asset in result.ChildAssets)
+            {
+                asset.ParentAssetIds.Should().Contain(parentId);
+            }
         }
     }
 }
