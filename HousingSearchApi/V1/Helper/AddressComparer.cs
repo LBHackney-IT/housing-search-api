1316e586ebf46223d4136c64e97daeb957ff9b24
Callum Macpherson <88662046+LBHCallumM@users.noreply.github.com>
2023-05-31 14:43:29 +0100

    Resolve exception in search sorting (#204)
    * Handle index out of range exception
    
    * Add tests
    
    * Include AddressLine2 when extracting address
    
    * Update tests
    
    * DF

--- HousingSearchApi/V1/Helper/AddressComparer.cs
+++ HousingSearchApi/V1/Helper/AddressComparer.cs
@@ -20,16 +20,44 @@ namespace HousingSearchApi.V1.Helper
                 }
             }
 
-            var addressline1_1 = address1.AddressLine1.Split(' ');
-            var addressline1_2 = address2.AddressLine1.Split(' ');
+            var address1Parts = ExtractAddressParts(address1);
+            var address2Parts = ExtractAddressParts(address2);
 
-            if (addressline1_1[1] == addressline1_2[1]
-                && int.TryParse(addressline1_1[0], out var house1)
-                && int.TryParse(addressline1_2[0], out var house2))
+            // prevent IndexOutOfRange exception (addressLine1 contains no spaces)
+            if (address1Parts.Count >= 2 && address2Parts.Count >= 2)
             {
-                return house1 - house2;
+                // if the street is the same, try compare house number
+                if (address1Parts[1] == address2Parts[1]
+                    && int.TryParse(address1Parts[0], out var house1)
+                    && int.TryParse(address2Parts[0], out var house2))
+                {
+                    return house1 - house2;
+                }
+
+                return address1Parts[1].CompareTo(address2Parts[1]);
             }
-            return addressline1_1[1].CompareTo(addressline1_2[1]);
+
+            // default sorting - compare addresssLine1 alphabetically
+            return address1.AddressLine1.CompareTo(address2.AddressLine1);
+        }
+
+        private static List<string> ExtractAddressParts(AssetAddress address)
+        {
+            var addressParts = new List<string> { };
+
+            // the relevant parts of the address will be in lines 1 and 2
+
+            if (!string.IsNullOrWhiteSpace(address.AddressLine1))
+            {
+                addressParts.AddRange(address.AddressLine1.Split(' '));
+            }
+
+            if (!string.IsNullOrWhiteSpace(address.AddressLine2))
+            {
+                addressParts.AddRange(address.AddressLine2.Split(' '));
+            }
+
+            return addressParts;
         }
 
         private static int? TryParseFlatNumber(AssetAddress address)
