131885dde0de799defe7847a798bdbe7be4899a7
Adam <adam.tracy20@gmail.com>
2024-09-23 09:21:01 +0100

    Improve search v2

--- HousingSearchApi/V2/Gateways/SearchGateway.cs
+++ HousingSearchApi/V2/Gateways/SearchGateway.cs
@@ -1,7 +1,8 @@
-using HousingSearchApi.V2.Gateways.Interfaces;
-using Nest;
+using System;
 using System.Threading.Tasks;
 using HousingSearchApi.V2.Domain.DTOs;
+using HousingSearchApi.V2.Gateways.Interfaces;
+using Nest;
 
 namespace HousingSearchApi.V2.Gateways;
 
@@ -17,22 +18,48 @@ public class SearchGateway : ISearchGateway
     public async Task<SearchResponseDto> Search(string indexName, SearchParametersDto searchParams)
     {
         var searchResponse = await _elasticClient.SearchAsync<object>(s => s
-                .Index(indexName)
-                .Query(q => q
-                    .SimpleQueryString(qs => qs
-                        .Fields("*")
-                        .Query(searchParams.SearchText)
+            .Index(indexName)
+            .Query(q => q
+                .Bool(b => b
+                    .Should(
+                        MultiMatchSingleField(searchParams.SearchText, boost: 6),
+                        MultiMatchAcrossFields(searchParams.SearchText, boost: 2)
                     )
                 )
-                .Size(searchParams.PageSize)
-                .From((searchParams.PageNumber - 1) * searchParams.PageSize)
-                .TrackTotalHits(true)
+            )
+            .Size(searchParams.PageSize)
+            .From((searchParams.PageNumber - 1) * searchParams.PageSize)
+            .TrackTotalHits()
         );
 
         return new SearchResponseDto
         {
             Documents = searchResponse.Documents,
-            Total = searchResponse.HitsMetadata.Total.Value
+            Total = searchResponse.HitsMetadata.Total.Value,
         };
     }
+
+
+    private Func<QueryContainerDescriptor<object>, QueryContainer> MultiMatchSingleField(string searchText, double boost) =>
+        should => should
+            .MultiMatch(mm => mm
+                .Fields("*")
+                .Query(searchText)
+                .Type(TextQueryType.BestFields) // High score if all terms are in the same field
+                .Operator(Operator.And) // All terms must be in the same field
+                .Fuzziness(Fuzziness.Auto) // Allow for some typos
+                .Boost(boost)
+            );
+
+
+    private Func<QueryContainerDescriptor<object>, QueryContainer> MultiMatchAcrossFields(string searchText, double boost) =>
+        should => should
+            .MultiMatch(mm => mm
+                .Fields("*")
+                .Query(searchText)
+                .Type(TextQueryType.CrossFields) // High score if all terms are in any field
+                .Operator(Operator.Or) // Terms can be across fields
+                .Boost(boost)
+            );
+
 }
