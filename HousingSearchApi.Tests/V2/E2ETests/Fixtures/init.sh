3e126740fc1c21d4a6b5f5f6c44190cbc0aca5a8
Adam <adam.tracy20@gmail.com>
2024-10-16 11:35:33 +0000

    No later build

--- HousingSearchApi.Tests/V2/E2ETests/Fixtures/init.sh
+++ HousingSearchApi.Tests/V2/E2ETests/Fixtures/init.sh
@@ -8,6 +8,11 @@ until curl -s test-elasticsearch:9200/_cluster/health?wait_for_status=green | gr
     echo "Waiting for Elasticsearch to be ready..."
 done
 
+# Set auto create index to true
+curl --silent --output /dev/null -s -XPUT 'test-elasticsearch:9200/_cluster/settings' \
+     -H "Content-Type: application/json" \
+     --data-binary '{"persistent": {"action.auto_create_index": "true"}}'
+
 echo "Elasticsearch is up and running - performing bulk data insertion"
 
 # Perform bulk insert using the Bulk API
