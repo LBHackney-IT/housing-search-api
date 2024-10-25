557e5a88a572e117ec08f9e089ff3e458f2a2f23
Adam <adam.tracy20@gmail.com>
2024-10-25 17:16:32 +0100

    V2 improvements

--- HousingSearchApi/V2/Gateways/SearchGateway.cs
+++ HousingSearchApi/V2/Gateways/SearchGateway.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using HousingSearchApi.V2.Domain.DTOs;
 using HousingSearchApi.V2.Gateways.Interfaces;
@@ -17,26 +18,55 @@ public class SearchGateway : ISearchGateway
 
     public async Task<SearchResponseDto> Search(string indexName, SearchParametersDto searchParams)
     {
-        string addressSearchFieldName;
+        var shouldOperations = new List<Func<QueryContainerDescriptor<object>, QueryContainer>>();
+        var defaultShouldOperations = new[]
+        {
+            SearchOperations.MultiMatchSingleField(searchParams.SearchText, boost: 6),
+            SearchOperations.MultiMatchCrossFields(searchParams.SearchText, boost: 2),
+            SearchOperations.MultiMatchMostFields(searchParams.SearchText, boost: 1)
+        };
+
         if (indexName == "assets")
-            addressSearchFieldName = "assetAddress.addressLine1";
+        {
+            var addressFieldName = "assetAddress.addressLine1";
+            shouldOperations.AddRange(new[]
+            {
+                SearchOperations.MatchPhrasePrefix(searchParams.SearchText, fieldName: addressFieldName, boost: 10),
+                SearchOperations.WildcardMatch(searchParams.SearchText, fieldName: addressFieldName, boost: 5),
+            });
+            shouldOperations.AddRange(defaultShouldOperations);
+        }
         else if (indexName == "tenures")
-            addressSearchFieldName = "tenuredAsset.fullAddress";
+        {
+            var addressFieldName = "tenuredAsset.fullAddress";
+            var nameFields = new List<string> { "householdMembers.fullName" };
+            shouldOperations
+            .AddRange(new[]
+            {
+                SearchOperations.SearchWithWildcardQuery(searchParams.SearchText, boost: 10, fields: nameFields),
+                SearchOperations.MatchPhrasePrefix(searchParams.SearchText, fieldName: addressFieldName, boost: 10),
+                SearchOperations.WildcardMatch(searchParams.SearchText, fieldName: addressFieldName, boost: 5),
+            });
+            shouldOperations.AddRange(defaultShouldOperations);
+        }
         else if (indexName == "persons")
-            addressSearchFieldName = "tenures.assetFullAddress";
-        else
-            throw new Exception($"Index name '{indexName}' is not supported");
+        {
+            var nameFields = new List<string> { "firstname", "surname" };
+            shouldOperations.AddRange(new[]
+            {
+                SearchOperations.SearchWithWildcardQuery(searchParams.SearchText, boost: 10, fields: nameFields),
+                SearchOperations.SearchWithExactQuery(searchParams.SearchText, boost: 6, fields: nameFields),
+            });
+        }
+        else // default
+            shouldOperations.AddRange(defaultShouldOperations);
+
 
         var searchResponse = await _elasticClient.SearchAsync<object>(s => s
             .Index(indexName)
             .Query(q => q
                 .Bool(b => b
-                    .Should(
-                        MatchPhrasePrefix(searchParams.SearchText, boost: 10, fieldName: addressSearchFieldName),
-                        MultiMatchSingleField(searchParams.SearchText, boost: 6),
-                        MultiMatchCrossFields(searchParams.SearchText, boost: 2),
-                        MultiMatchMostFields(searchParams.SearchText, boost: 1)
-                    )
+                    .Should(shouldOperations)
                 )
             )
             .MinScore(25)
@@ -54,51 +84,4 @@ public class SearchGateway : ISearchGateway
             Total = searchResponse.HitsMetadata.Total.Value,
         };
     }
-
-    // Score for matching a value which starts with the search text
-    private Func<QueryContainerDescriptor<object>, QueryContainer>
-        MatchPhrasePrefix(string searchText, double boost, string fieldName) =>
-        should => should
-            .MatchPhrasePrefix(mp => mp
-                .Field(fieldName)
-                .Query(searchText)
-                .Boost(boost)
-            );
-
-    // Score for matching a single (best) field
-    private Func<QueryContainerDescriptor<object>, QueryContainer>
-        MultiMatchSingleField(string searchText, double boost) =>
-        should => should
-            .MultiMatch(mm => mm
-                .Fields("*")
-                .Query(searchText)
-                .Type(TextQueryType.BestFields)
-                .Operator(Operator.And)
-                .Fuzziness(Fuzziness.Auto)
-                .Boost(boost)
-            );
-
-    // Score for matching the combination of many fields
-    private Func<QueryContainerDescriptor<object>, QueryContainer>
-        MultiMatchCrossFields(string searchText, double boost) =>
-        should => should
-            .MultiMatch(mm => mm
-                .Fields("*")
-                .Query(searchText)
-                .Type(TextQueryType.CrossFields)
-                .Operator(Operator.Or)
-                .Boost(boost)
-            );
-
-    // Score for matching a high number (quantity) of fields
-    private Func<QueryContainerDescriptor<object>, QueryContainer> MultiMatchMostFields(string searchText, double boost) =>
-        should => should
-            .MultiMatch(mm => mm
-                .Fields("*")
-                .Query(searchText)
-                .Type(TextQueryType.MostFields)
-                .Operator(Operator.Or)
-                .Fuzziness(Fuzziness.Auto)
-                .Boost(boost)
-            );
 }
