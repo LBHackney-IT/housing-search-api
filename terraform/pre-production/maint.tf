fc0e7a84bcc9764d34dc7f617d06f45753b403f2
LBHTKarki <tuomo.karki@hackney.gov.uk>
2025-05-20 18:39:32 +0100

    fix aws provider config

--- terraform/pre-production/maint.tf
+++ terraform/pre-production/maint.tf
@@ -1,6 +1,14 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 2.0"
+    }
+  }
+}
+
 provider "aws" {
-  region  = "eu-west-2"
-  version = "~> 2.0"
+  region = "eu-west-2"
 
   default_tags {
     tags = {
