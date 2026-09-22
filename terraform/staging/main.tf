97bb6f5e6cad0e4d8e07b2a5b5064356c72ab359
Adam <adam.tracy20@gmail.com>
2026-09-22 11:16:01 +0100

    Upgrade staging es_version to 7.10

--- terraform/staging/main.tf
+++ terraform/staging/main.tf
@@ -42,7 +42,7 @@ module "elasticsearch_db_staging" {
   domain_name      = "housing-search-api-es"
   subnet_ids       = data.aws_subnet_ids.staging.ids
   project_name     = "housing-search-api"
-  es_version       = "7.8"
+  es_version       = "7.10"
   encrypt_at_rest  = "true"
   instance_type    = "t3.small.elasticsearch"
   instance_count   = "2"
